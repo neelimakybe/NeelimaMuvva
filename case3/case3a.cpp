c274bff3b40ed027bbdd59a095210c9ccbfc5934
Neelima <54301352+neelimakybe@users.noreply.github.com>
2020-04-12 13:17:15 +0530

    Update case3a.cpp

--- case3/case3a.cpp
+++ case3/case3a.cpp
@@ -4,20 +4,53 @@ File Name:case3a.cpp
 Date:09-04-2020
 */
 #include<iostream>
+
+#include<string.h>
+
 using namespace std;
-//Global variable
-int g;
-int main (int argc,char *argv[]) {
-    if(argc==2)
-    {
-        cout<<"usage:./a.out"<<endl;
-    }
-    else
-    {
-        //local variable
-   int a=10,b=15;
-   g = a + b;
-   cout << g;
-   return 0;
+
+int age=21;//Declared Global variable
+
+void display()//Function Declaration
+
+{
+
+    int age=18;//Local Variable Declaration which will be used for this particulor function
+
+    cout<<"A Women can marry at the age:"<<age<<endl;//age can be accessed by global declared variable
+
 }
+
+int main(int argc,char **argv)
+
+{
+
+    
+
+    if(argc == 2 && strcmp(argv[1], "-h")==0) //help command 
+
+   
+
+    {
+
+        cout<<"The main purpose of this program is to how to use & Declare the Local & Global scope variable"<<endl;
+
+    }
+
+    else
+
+    {
+
+        display();//displaying the function
+
+        
+
+        cout<<"A men can marry at the age:"<<age<<endl;//Here age is taken globally
+
+        
+
+    }
+
+    return 0;
+
 }
