746591aa614c501e1da89666685516a9b6164158
Neelima <54301352+neelimakybe@users.noreply.github.com>
2020-04-12 10:06:23 +0530

    Add files via upload

--- case2/case2.cpp
+++ case2/case2.cpp
@@ -1,1 +1,55 @@
+/*Name of the Program:CPP Program to find sum of squares of first n natural numbers
+Author Name:Neelima Muvva
+File Name:case2.cpp
+Date:10-04-2020
+*/
+
+
+#include<iostream>
+
+#include<stdlib.h>
+
+using namespace std;
+
+//Returns the  value of sum of the squares  of first n natural numbers 
+
+
+
+int square(int iW)
+
+{
+
+int iOu;
+
+iOu= (iW*(iW+1)*(2*iW+1))/6;
+
+cout<<"output"<<iOu<<endl;
+
+}
+
+
+
+int main(int argc,char*argv[])
+
+{
+
+int iM;
+
+if(argc==2)
+
+cout<<"usage:./a.out arg1"<<endl;
+
+else
+
+{
+
+cin>>iM;
+
+square(iM);
+
+}
+
+}
+
+
 
