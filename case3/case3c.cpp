537389f9dbd380ac5b69b67aed399b7110dea58a
Neelima <54301352+neelimakybe@users.noreply.github.com>
2020-04-27 11:16:16 +0530

    Update case3c.cpp

--- case3/case3c.cpp
+++ case3/case3c.cpp
@@ -4,63 +4,35 @@ File Name:case3c.cpp
 Date:09-04-2020
 */
 #include<iostream>
-
 #include<string.h>
-
 using namespace std;
-
 void display()//Function Declaration
-
 {
-
-    unsigned char a='c'; //unsigned character Modifier
-
-    signed char b='c'; //signed character Modifier
-
-    unsigned int c=2500;//unsigned integer Modifier
-
-    signed int d= -2500;//signed integer Modifier
-
-    unsigned short int e=2500;//unsigned shor integer Modifier
-
-    signed short int f= -5000;//signed short integer Modifier
-
-    unsigned long int g=5000;//unsigned long integer Modifier
-
-    signed long int h=-2500;//signed long integer Modifier
-
-    long double i=6000;//Long double Modifier 
-
-    cout<<a<<endl<<b<<endl<<c<<endl<<d<<endl<<e<<endl<<f<<endl<<g<<endl<<h<<endl<<i;
-
+    int numb;
+    short int sint;
+    unsigned short usint;
+    signed short ssint;
+    long int lint;
+    unsigned long uslint;
+    cout<<"                     MODIFIER TYPES                         "<<endl;
+    cout<<"Enter int value: ";
+    cin>>numb;
+    sint=usint=ssint=lint=uslint=numb;
+    cout<<"short int   : "<<sint<<endl;
+    cout<<"unsigned short   : "<<usint<<endl;
+    cout<<"signed short: "<<ssint<<endl;
+    cout<<"long int         : "<<lint<<endl;
+    cout<<"unsigned long  : "<<uslint<<endl;
 }
-
-int main(int argc,char **argv)
-
+int main(int argc,char *argv[])
 {
-
-    
-
-    if(argc == 2 && strcmp(argv[1], "-h")==0) //help command 
-
-   
-
+    if(argc == 2 && strcmp(argv[1], "-h")==0) //help command
     {
-
         cout<<"The main purpose of this program is to how to use & Declare Types of Modifiers"<<endl;
-
     }
-
     else
-
     {
-
         display();
-
     }
-
     return 0;
-
 }
- 
-
